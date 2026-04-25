60247632ded7c6aa8061ef1ae6ebdc8135f9f4bc
Ryan Enriq <ryanenriq.dev@gmail.com>
2026-04-25 17:51:59 -0300

    feat: criacao da funcao login em javascript e ligação com o frontend

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,45 @@ document.addEventListener("DOMContentLoaded", () => {
         cartaoAutenticacao.classList.remove('mostrar-cadastro'); 
     });
 });
+
+// Lógica de Login
+async function login(event) {
+    // Impede o envio padrão do formulário
+    event.preventDefault();
+
+    // Pega os valores dos campos de email e senha
+    let email = document.querySelector("#login-email")
+    let email_value = email.value;
+    let password = document.querySelector("#login-senha");
+    let password_value = password.value;
+    
+    // Busca o arquivo JSON e converte para um objeto JavaScript
+    const resposta = await fetch("../data/users.json");
+    const users = await resposta.json();
+
+    // Procura o usuário com o email fornecido
+    const email_encontrado = users.find(user => user.email === email_value);
+    
+    // Se encontrar o email, verifica se a senha bate com a senha armazenada
+    if (email_encontrado) {
+        if (email_encontrado.password_hash === password_value) {
+            alert("Login bem-sucedido!");
+            email.value = "";
+            password.value = "";
+            return current_user = email_encontrado;
+        }
+        else {
+            alert("Senha incorreta!");
+            password.value = "";
+        }
+    }
+    else {
+        alert("Email não encontrado!");
+        email.value = "";
+        password.value = "";
+    }
+}
+
+// Selecão do botão de Login e evento que chama a função quando o botão for clicado
+const btn_entrar = document.querySelector("#botao-login");
+btn_entrar.addEventListener("click", login);
