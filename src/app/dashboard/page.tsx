cf5e7d1429f3bd00e68395269c6cfe5656e36733
Ryan Enriq <ryanenriq.dev@gmail.com>
2026-06-18 00:46:29 -0300

    feat: componentizando cards de estágios recomendados

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,12 @@
 
 import CabecalhoBoasVindas from "@/components/CabecalhoBoasVindas";
 import CardInformativo from "@/components/CardInformativo";
+import { CardEstagioRecomendado, CardNenhumEstagioDisponivel } from "@/components/CardEstagioRecomendado";
 import { cardsAluno, cardsOrientador, cardsCoordenador } from "@/data/cards-dashboard";
 import { useAuth } from "@/contexts/AuthContext";
 import { listarEstagiosRecomendados } from "@/lib/supabase/functions-supabase";
 import { useEffect, useState } from "react";
 import { EstagioRecomendado } from "@/lib/supabase/interface";
-import { SearchX } from 'lucide-react';
 
 export default function Dashboard() {
   const { usuario, setUsuario } = useAuth();
@@ -89,29 +89,12 @@ export default function Dashboard() {
 
         { estagiosDisponiveis.length > 0 ? (
           estagiosDisponiveis.map((estagio) => (
-            <div key={estagio.id} className="bg-white p-5 rounded-2xl shadow-sm space-y-1">
-              <h2 className="text-xl font-semibold -mb-1">{estagio.titulo}</h2>
-              <h4 className="text-slate-600 font-semibold text-[0.9rem]">{estagio.empresa}</h4>
-
-              <div className="flex text-[0.7rem] font-medium text-slate-700 gap-2 mb-5">
-                <p className="bg-gray-200 rounded-sm p-1">{estagio.competencia}</p>
-                <p className="bg-gray-200 rounded-sm p-1">{estagio.vagas ? estagio.vagas + ' Vagas' : 'Quantidade de Vagas Indefinidas'}</p>
-              </div>
-              <button className="border border-blue-600 p-2 w-full rounded-lg text-blue-600 text-sm font-semibold hover:bg-blue-600 hover:text-white">Ver Detalhes</button>
-            </div>
-            
-          )
-          ))
-          :
+            <CardEstagioRecomendado key={estagio.id} estagio={estagio} />   
+          ))) :
           (
-          <div className="flex flex-col bg-white p-12 rounded-2xl shadow-sm items-center text-center">
-            <SearchX className="w-18 h-18 bg-gray-200 text-slate-500 p-3 rounded-full shrink-0 mb-3" />
-            <h1 className="text-2xl font-semibold">Nenhuma vaga disponível no momento</h1>
-            <p>Fique atento! Novas oportunidades podem surgir a qualquer instante.</p>
-          </div>
-          ) }
+            <CardNenhumEstagioDisponivel />
+          )}
       </section>
-
       </div>
 
       
