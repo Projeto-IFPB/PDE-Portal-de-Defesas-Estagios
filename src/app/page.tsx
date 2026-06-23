8b1fbfb3fc996f0666c7590eb6e99b0b5cc88738
Diogo Silva <diogoss.dev@gmail.com>
2026-06-23 02:16:32 -0300

    Criando layout de interatividade inicial para login e cadastro.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
 export default function Home() {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <>
-      <h1>Tela de Login/Cadastro</h1>
-    </>
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      {/* Container Principal */}
+      <div 
+        className={`relative flex flex-col-reverse md:flex-row w-full max-w-5xl bg-white shadow-2xl rounded-2xl overflow-hidden min-h-[600px] transition-all duration-500 ${
+          !isLogin ? "md:flex-row-reverse" : ""
+        }`}
+      >
+
+        <div className="w-full md:w-1/2 p-8 sm:p-12 flex flex-col justify-center">
+
+          <h2 className="text-2xl font-bold text-center text-gray-800">
+            {isLogin ? "Área de Login" : "Área de Cadastro"}
+          </h2>
+        </div>
+
+        <div className="w-full md:w-1/2 bg-[#1f499b] text-white p-8 sm:p-12 flex flex-col justify-center items-center text-center transition-all duration-500">
+
+           <p>Painel Azul</p>
+        </div>
+      </div>
+    </main>
   );
 }
